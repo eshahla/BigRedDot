c7cf05b7da28b25f72aaf0ed71fd62e992113ded
eshahla <ess9213@yahoo.com>
2015-11-03 09:20:44 -0800

    4 commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,15 @@ class UsersController < ApplicationController
   end
  end
 
- private
-   def user_params
-    params.require(:user).permit( :email, :password, :password_confirmation)
-   end
-
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+
+ private
+   def user_params
+    params.require(:user).permit( :email, :password, :password_confirmation)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   root 'users#index'
   resources :users
+  get 'signup' => 'users#new'
 
-  get 'login', to: 'sessions#new'
+  get 'login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
   resources :sessions, only: [:new, :create, :destroy]
 
 end

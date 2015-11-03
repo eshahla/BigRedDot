e468df8072f382f7c84fd636bdde929e18a0c60f
eshahla <ess9213@yahoo.com>
2015-11-02 20:06:43 -0800

    2 commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create({first_name:"Elnaz", last_name:"Shahla", email:"ess9213@yahoo.com", password:"elnazshahla", address1:"1229 12th st", address2:"apt 8", state:"CA", city:"LA", zip:90401, phone_number:4731959})

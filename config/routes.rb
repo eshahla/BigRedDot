20938891ef9f1e848878dde688b954dda16f5eb7
eshahla <ess9213@yahoo.com>
2015-11-05 12:42:10 -0800

    8 commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :users
   get 'join' => 'users#new'
 
+  get 'reviews' => 'users#reviews'
+
   get 'login' => 'sessions#new'
+
+  get 'how_it_works' => 'users#how_it_works'
   delete 'logout' => 'sessions#destroy'
   resources :sessions, only: [:new, :create, :destroy]
   resources :products

e78664fd458fff038cd78e33eac7589f9a35fa0e
eshahla <ess9213@yahoo.com>
2015-11-05 23:28:38 -0800

    tampoon paddie

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,21 @@ Rails.application.routes.draw do
 
   get 'login' => 'sessions#new'
 
+  get 'tampons' => 'products#tampons'
+
+  get 'pads' => 'products#pads'
+
+  get 'customize' => 'products#customize'
+
   get 'how_it_works' => 'users#how_it_works'
+
+  get 'contact' => 'users#contact'
   delete 'logout' => 'sessions#destroy'
   resources :sessions, only: [:new, :create, :destroy]
   resources :products
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
+
+  post 'users/thankyou' => 'users#thankyou'
+
 end
